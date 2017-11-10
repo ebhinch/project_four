b5da1dae767ffb537bb32642491c4e738987ef25
ebhinch <erica.hinchman@gmail.com>
2017-11-10 15:55:47 -0500

    built out seeds for towns and was able to retrieve them using get request in postman

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Town.destroy_all
+
+Town.create({
+    name: "Telluride, Colorado",
+    lat: 37.9375,
+    lng: 107.8123,
+    description: "Telluride, a former Victorian mining town in Colorado’s Rocky Mountains, is set in a box canyon amid forested peaks at the base of a popular ski-and-golf resort. The town’s historic district houses landmarks like the Sheridan Opera House, a performing-arts venue originally built in 1913, and the Telluride Historical Museum, which showcases local history in a converted hospital built in 1896.",
+    population: 2444,
+    image: "http://www.momsformomscommunities.com/wp-content/uploads/2011/05/telluride-town-center.jpg"
+})
+
+Town.create({
+    name: "Taos, New Mexico",
+    lat: 36.4072,
+    lng: 105.5731,
+    description: "Taos is a town in northern New Mexico’s high desert, bounded by the Sangre de Cristo Mountains. It’s known for historic adobe buildings such as Taos Pueblo, a multistory adobe complex inhabited by Native Americans for centuries. A longtime artist colony, Taos also offers many galleries and museums showcasing regional artwork, including the Harwood Museum of Art and the Taos Art Museum.",
+    population: 5736,
+    image: "http://adventure.nationalgeographic.com/2009/10/adventure-towns/img/taos-ms-450.jpg"
+})
+
+Town.create({
+    name: "Jackson, Wyoming",
+    lat: 43.4799,
+    lng: 110.7624,
+    description: "Jackson is a town in Wyoming’s Jackson Hole valley, home to 3 ski areas: Jackson Hole Mountain Resort, Snow King Mountain Resort and Grand Targhee Resort. The Town Square features arches made of shed antlers from the nearby National Elk Refuge. The National Museum of Wildlife Art has works by Andy Warhol and Georgia O’Keeffe. North are the peaks of Grand Teton National Park, as well as vast Yellowstone National Park.",
+    population: 10529,
+    image: "https://www.etraveltrips.com/wp-content/uploads/2011/11/Jackson-Hole-Wyoming.jpg"
+})
+
+Town.create({
+    name: "Ogden, Utah", 
+    lat: 41.2230,
+    lng: 111.9738,
+    description: "There are no crowds, yet Ogden's teeming with boundless recreation, exciting attractions and fabulous events year round amid stunning natural beauty. Locals are passionate about the area and welcoming to visitors who come to explore the colorful character of Ogden City through a wide range of historic attractions, sites and buildings that celebrate the legends of this once rugged and still vibrant Crossroads of the West.",
+    population: 86701,
+    image: "https://media.deseretdigital.com/file/001f088444?crop=top:0|left:0|width:1260|height:670|gravity:Center&quality=55&interlace=none&resize=width:1260&order=resize,crop&c=14&a=e0f131f0"
+})
