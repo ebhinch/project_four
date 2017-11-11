0887df5b36e0f4ad08ac38cae975bbeb9881189a
ebhinch <erica.hinchman@gmail.com>
2017-11-11 16:42:33 -0500

    added create route for events

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,6 +1,16 @@
 class Api::EventsController < ApplicationController
 
-   
 
+    def create
+        @event = Event.create(event_params)
+        render json: @event
+    end
 
-end
+    private
+    
+        def event_params
+            params.require(:event).permit(:name, :date, :season, :image, :town_id)
+        end
+    
+    end
+     
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
   mount_devise_token_auth_for 'User', at: 'auth'
-  
+
   namespace :api do 
     resources :towns, only: [:index, :show]
-    resources :events, only: [:index, :show]
+    resources :events, only: [:index, :show, :create]
     resources :comments, only: [:create, :update, :destroy, :show, :index]
     
   end
