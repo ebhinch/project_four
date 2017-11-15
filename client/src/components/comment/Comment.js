4ca955f6bae26815114808a59465eac75b78949e
ebhinch <erica.hinchman@gmail.com>
2017-11-15 17:31:28 -0500

    added time since comments posted to each comment. am not yet using time zone

--- client/src/components/comment/Comment.js
+++ client/src/components/comment/Comment.js
@@ -1,15 +1,17 @@
 import React from 'react';
-
+import moment from 'moment';
+import 'moment-timezone';
 
 
 
 const Comment = (props) => {
     return (
         <div>
-            <h1>THIS IS THE COMMENT COMPONENT - SHOWING COMMENTS FOR INDIVIDUAL EVENTS</h1>
-            <h4>{props.title}</h4>
+            <h3>{props.title}</h3>
             <h5>{props.text}</h5>
-      
+            <moment></moment>
+            Posted: {moment(props.created_at).fromNow()}
+
         </div>
     );
 };
