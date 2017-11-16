eeb17a42e2a30199d063d50904b06c110cad9a6e
ebhinch <erica.hinchman@gmail.com>
2017-11-16 16:40:06 -0500

    styled login page

--- client/src/components/SignUpLogIn.js
+++ client/src/components/SignUpLogIn.js
@@ -2,6 +2,45 @@
 
 import React, { Component } from 'react'
 import { Redirect } from "react-router-dom"
+import styled from 'styled-components'
+
+const FormContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-top: 5vh;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    width: 70%;
+    margin-left: 15%;
+    margin-right: 15%;
+    padding-top: 7%;
+    padding-left: 10%;
+    height: 30vh;
+ 
+ 
+`
+
+const FormText = styled.div`
+ 
+    font-family: 'Roboto', sans-serif;
+    font-weight: bold;
+    font-size: 20px;
+   
+    button {
+        border-radius: 3px;
+        padding: 0.25em 1em;
+        background: transparent;
+        font-family: 'Alegreya Sans SC', sans-serif;
+         
+        align-items: center;
+        text-align: center;        
+    };
+    div {
+        margin-top: 3px;
+        margin-bottom: 3px;
+    }
+   
+
+`
 
 class SignUpLogIn extends Component {
 
@@ -13,8 +52,8 @@ class SignUpLogIn extends Component {
     }
 
     handleRedirect = () => {
-        this.setState({toggleRedirect: !this.state.toggleRedirect})
-        }
+        this.setState({ toggleRedirect: !this.state.toggleRedirect })
+    }
 
     signUp = (event) => {
         event.preventDefault()
@@ -47,27 +86,35 @@ class SignUpLogIn extends Component {
                 <Redirect to="/" />
             )
         }
-        
+
         return (
             <div>
-                <form>
-                    <div>
-                        <label htmlFor="email">E-mail: </label>
-                        <input onChange={this.handleChange} type="text" name="email" value={this.state.email} />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password: </label>
-                        <input onChange={this.handleChange} type="password" name="password" value={this.state.password} />
-                    </div>
-                    <div>
-                        <label htmlFor="password_confirmation">Confirm Password: </label>
-                        <input onChange={this.handleChange} type="password" name="password_confirmation"
-                            value={this.state.password_confirmation} />
-                    </div>
+                <FormContainer>
 
-                    <button onClick={this.signUp}>Sign Up</button>
-                    <button onClick={this.signIn}>Log In</button>
-                </form>
+
+                    <FormText>
+
+                        <form>
+                            <div>
+                                <label htmlFor="email">E-mail: </label>
+                                <input onChange={this.handleChange} type="text" name="email" value={this.state.email} />
+                            </div>
+                            <div>
+                                <label htmlFor="password">Password: </label>
+                                <input onChange={this.handleChange} type="password" name="password" value={this.state.password} />
+                            </div>
+                            <div>
+                                <label htmlFor="password_confirmation">Confirm Password: </label>
+                                <input onChange={this.handleChange} type="password" name="password_confirmation"
+                                    value={this.state.password_confirmation} />
+                            </div>
+                            <button onClick={this.signIn}>Log In</button>
+                            <button onClick={this.signUp}>Sign Up</button>
+
+                        </form>
+
+                    </FormText>
+                </FormContainer>
             </div>
         )
     }
--- client/src/components/town/Town.js
+++ client/src/components/town/Town.js
@@ -59,11 +59,12 @@ const TownBody = styled.div`
         width: 100%;
         margin: 0 auto;
         height: 300px;
+        margin-top: 10px;
     }
 
   `
 
-  const Buttons = styled.div`
+const Buttons = styled.div`
     display: flex;
   `
 
@@ -156,44 +157,44 @@ class Town extends Component {
     render() {
         return (
             <TownContainer>
-            <TownBody>
+                <TownBody>
 
-                <img src={this.state.town.image} />
-                <ShadowDiv>
-                <h2>{this.state.town.name} </h2>
+                    <img src={this.state.town.image} />
+                    <ShadowDiv>
+                        <h2>{this.state.town.name} </h2>
 
-                <h4>{this.state.town.description}</h4>
+                        <h4>{this.state.town.description}</h4>
 
-                <h4>Population: </h4>
-                <NumberWithCommas number={this.state.town.population} />
+                        <h4>Population: </h4>
+                        <NumberWithCommas number={this.state.town.population} />
 
-                <h4>Elevation: </h4>
-                <NumberWithCommas number={this.state.elevation} />
-                </ShadowDiv>
-                <ShadowDiv>
-                <EventTextIntro town={this.state.town} />
-                </ShadowDiv>
-                <Buttons>
-                <button onClick={this.toggleShowWS}>Winter | Spring</button>
+                        <h4>Elevation: </h4>
+                        <NumberWithCommas number={this.state.elevation} />
+                    </ShadowDiv>
+                    <ShadowDiv>
+                        <EventTextIntro town={this.state.town} />
+                    </ShadowDiv>
+                    <Buttons>
+                        <button onClick={this.toggleShowWS}>Winter | Spring</button>
 
-                {this.state.showWS ? <WSEventList town={this.state.town} events={this.state.town.events} townId={this.state.town.id} deleteEvent={this.deleteEvent} /> : null}
+                        {this.state.showWS ? <WSEventList town={this.state.town} events={this.state.town.events} townId={this.state.town.id} deleteEvent={this.deleteEvent} /> : null}
 
-                <button onClick={this.toggleShowSF}>Summer | Fall </button>
+                        <button onClick={this.toggleShowSF}>Summer | Fall </button>
 
-                {this.state.showSF ? <SFEventList town={this.state.town} events={this.state.town.events} townId={this.state.town.id} deleteEvent={this.deleteEvent} /> : null}
-                </Buttons>
-                <SubmitNewEventText town={this.state.town} />
+                        {this.state.showSF ? <SFEventList town={this.state.town} events={this.state.town.events} townId={this.state.town.id} deleteEvent={this.deleteEvent} /> : null}
+                    </Buttons>
+                    <SubmitNewEventText town={this.state.town} />
 
-                <button onClick={this.toggleShowCreate}>Add to {this.state.town.name}'s happenings</button>
+                    <button onClick={this.toggleShowCreate}>Add to {this.state.town.name}'s happenings</button>
 
-                <div>
-                    {this.state.showCreate ? <CreateEvent townId={this.state.town.id} toggleShowCreate={this.toggleShowCreate} pushEvents={this.pushEvents} /> : null}
-                </div>
-
-
+                    <div>
+                        {this.state.showCreate ? <CreateEvent townId={this.state.town.id} toggleShowCreate={this.toggleShowCreate} pushEvents={this.pushEvents} /> : null}
+                    </div>
 
 
-            </TownBody>
+
+
+                </TownBody>
             </TownContainer>
         );
     }
