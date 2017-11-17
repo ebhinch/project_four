3c54fd268606d76ca829054a3f385c26ad20aa0a
ebhinch <erica.hinchman@gmail.com>
2017-11-17 09:36:31 -0500

    adjusted width of sign in alert

--- client/src/components/comment/CommentSignInAlert.js
+++ client/src/components/comment/CommentSignInAlert.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import styled from 'styled-components'
 
-const AlertDiv = styled.div`
-    font-size: 10px;
+const AlertDiv = styled.span`
+    font-size: 12px;
 
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     background-color: #FF5D55;
     margin-top: 2px;
     padding: 3px;
-    width: 60%;
-    h2 {
-        margin: 0;
-    }
+    max-width: 250px;
 `
 
 
 const CommentSignInAlert = () => {
     return (
         <AlertDiv>
-            <h2>You must be logged in to leave a comment</h2>
+            <span>You must be logged in to leave a comment</span>
         </AlertDiv>
     );
 };
