d42b3e2f75bde13b7e219a3918d58bd7dcca8b6c
ebhinch <erica.hinchman@gmail.com>
2017-11-17 12:38:43 -0500

    added validations to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :town
   has_many :comments, dependent: :destroy
   # has_many :users, through: :comments
+  validates :name, presence: :true
+  validates :date, presence: :true
+  validates :season, presence: :true
+  validates :image, presence: :true
 end
