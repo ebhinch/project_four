ed4f423bf3dc5ddb19a6e74244fa0933d13ae501
ebhinch <erica.hinchman@gmail.com>
2017-11-28 14:02:02 -0500

    edited hover on footer and header

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,34 +5,24 @@ import styled from 'styled-components'
 
 const StyledFooter = styled.div`
     display: flex;
-    flex-direction: row;
-    flex-grow: 1;
+    justify-content: space-around;
 
 `
 
 const FooterItem = styled.div`
-    width: 100%;
     font-family: 'Alegreya Sans SC', sans-serif;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     margin: 0px;
     padding-bottom: 10px;
     padding-top: 10px;
-    ul {
-        display: flex;
-        justify-content: space-around;
-        list-style-type: none;
-        padding-top: 2px;
-        padding-bottom: 2px;
-        margin-top: 0px;
-        margin-bottom: 0px;
-        font-size: 18px;
-    };
+    padding-right: 5px;
+    padding-left: 5px;
+    font-size: 18px;
     a {
         color: #800D57;
         text-decoration: none;
     };
     a:hover {
-        font-size: 14px;
+        font-weight: bold;
     };
     a:visited {
         color: navy
@@ -42,12 +32,12 @@ const FooterItem = styled.div`
 const Footer = () => {
     return (
         <StyledFooter>
-            <FooterItem>
-                <ul>
-                    <li><Link to={"/"}>HOME</Link></li>
-                    <li><Link to={"/towns"}>TOWN ROSTER</Link></li>
-                </ul>
-            </FooterItem>
+      
+           
+            <FooterItem><Link to={"/"}>HOME</Link></FooterItem>
+            <FooterItem><Link to={"/towns"}>TOWN ROSTER</Link></FooterItem>
+               
+    
         </StyledFooter>
     );
 };
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,55 +5,51 @@ import styled from 'styled-components'
 
 const StyledHeader = styled.div`
     display: flex;
-    flex-direction: row;
-    flex-grow: 1;
-    height: 10%;
+    justify-content: space-around;
 `
 
 const HeaderItem = styled.div`
-    width: 100%;
     font-family: 'Alegreya Sans SC', sans-serif;
     margin: 0px;
     padding-bottom: 10px;
     padding-top: 10px;
-    ul {
-        display: flex;
-        justify-content: space-around;
-        list-style-type: none;
-        padding-top: 2px;
-        padding-bottom: 2px;
-        margin-top: 0px;
-        margin-bottom: 0px;
-        font-size: 12px;
-    };
+    padding-right: 5px;
+    padding-left: 5px;
+    font-size: 20px;
     a {
         color: #800D57;
         text-decoration: none;
     };
     a:hover {
-        font-size: 14px;
-    };
+        font-weight: bold;
+        };
     a:visited {
         color: navy
     };
-    span {
-        font-weight: bold;
-        font-size: 16px;
-    }
+`
+
+const Logo = styled.div`
+    margin: 0px;
+    padding-bottom: 10px;
+    padding-top: 10px;
+    padding-right: 5px;
+    padding-left: 5px;
+    font-weight: bold;
+    font-size: 20px;
+    font-family: 'Alegreya Sans SC', sans-serif;
 `
 
 const Header = () => {
     return (
         <StyledHeader>
-            <HeaderItem>
+
            
-                <ul>
-                <span>ROCK YOUR LIFE</span>
-                    <li><Link to={"/"}>HOME</Link></li>
-                    <li><Link to={"/towns"}>TOWN ROSTER</Link></li>
-                </ul>
-                
-            </HeaderItem>
+           
+                <Logo>ROCK YOUR LIFE</Logo>
+                    <HeaderItem><Link to={"/"}>HOME</Link></HeaderItem>
+                    <HeaderItem><Link to={"/towns"}>TOWN ROSTER</Link></HeaderItem>
+         
+  
         </StyledHeader>
     );
 };
